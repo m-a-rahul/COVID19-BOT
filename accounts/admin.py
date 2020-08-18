7770057dc3b43a0dfc7dd355e38da1b776f864f4
Rahul0700 <antorahul070@gmail.com>
2020-08-18 20:33:39 +0530

    Booking table creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Hospitalprofile,Testingprofile
+from .models import Hospitalprofile,Testingprofile,Booking
 
 # Register your models here.
 admin.site.register(Hospitalprofile)
 admin.site.register(Testingprofile)
+admin.site.register(Booking)
--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator,MaxLengthValidator
 from django.contrib.auth.models import User
-# Create your models here.
+
 class Hospitalprofile(models.Model):
     hospital = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
@@ -19,3 +19,11 @@ class Testingprofile(models.Model):
     pin_code = models.CharField(max_length=6,validators=[MinLengthValidator(6)])
     contact_no = models.CharField(max_length=10,validators=[MinLengthValidator(10)])
     availablity = models.PositiveSmallIntegerField()
+
+class Booking(models.Model):
+    naiveuser_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=1000)
+    pin_code = models.CharField(max_length=6,validators=[MinLengthValidator(6)])
+    contact_no = models.CharField(max_length=10,validators=[MinLengthValidator(10)])
+    booking = models.ForeignKey(User,on_delete=models.CASCADE)
