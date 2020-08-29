9e8c24b36fb4e62565b32116b447fecc4ae05d37
Rahul0700 <antorahul070@gmail.com>
2020-08-29 18:10:31 +0530

    Update forms for hospital/testing

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/hospital',views.Hospitalsignup,name='hsignup'),
     path('signup/testing',views.Testingsignup,name='tsignup'),
+    path('update/hospital',views.Hospitalupdate,name='hupdate'),
+    path('update/testing',views.Testingupdate,name='tupdate'),
     path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.utils.text import slugify
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Hospitalprofile,Testingprofile
 # Create your views here.
 def Hospitalsignup(request):
     if request.method == 'POST':
@@ -31,12 +32,24 @@ def Hospitalsignup(request):
                 messages.success(request,'Your user id is : %s'%code)
                 info_form.save()
                 details_form.save()
-                return HttpResponseRedirect(reverse('login'))
+                return HttpResponseRedirect(reverse('accounts:login'))
     else :
         hospital_form = HospitalCreateForm()
         hospital_profile_form = HospitalprofileForm()
     return render(request,'accounts/signup.html',{'form':hospital_form,
                                                   'profile_form':hospital_profile_form})
+
+def Hospitalupdate(request):
+    hospital_instance=Hospitalprofile.objects.get(hospital=request.user)
+    is_hospital = "True"
+    if request.method == 'POST':
+        hospital_profile_form = HospitalprofileForm(data=request.POST,instance=hospital_instance)
+        if hospital_profile_form.is_valid():
+            details_form = hospital_profile_form.save()
+    else :
+        hospital_profile_form = HospitalprofileForm(instance=hospital_instance)
+    return render(request,'accounts/update.html',{'form':hospital_profile_form,
+                                                   'is_hospital':is_hospital})
 
 def Testingsignup(request):
     if request.method == 'POST':
@@ -61,9 +74,21 @@ def Testingsignup(request):
             messages.success(request,'Your user id is : %s'%code)
             info_form.save()
             details_form.save()
-            return HttpResponseRedirect(reverse('login'))
+            return HttpResponseRedirect(reverse('accounts:login'))
     else :
         test_form = TestingCreateForm()
         test_profile_form = TestingprofileForm()
     return render(request,'accounts/signup.html',{'form':test_form,
                                                   'profile_form':test_profile_form})
+
+def Testingupdate(request):
+    is_hospital = "False"
+    testing_instance = Testingprofile.objects.get(testing=request.user)
+    if request.method == 'POST':
+        testing_profile_form = TestingprofileForm(data=request.POST,instance=testing_instance)
+        if testing_profile_form.is_valid():
+            details_form = testing_profile_form.save()
+    else :
+        testing_profile_form = TestingprofileForm(instance=testing_instance)
+    return render(request,'accounts/update.html',{'form':testing_profile_form,
+                                                  'is_hospital':is_hospital})
